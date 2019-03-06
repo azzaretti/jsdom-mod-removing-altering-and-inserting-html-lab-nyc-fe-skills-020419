67944793b271334a95869e492c6555cf7838345d
Greg Azzaretti <azzaretti.greg@gmail.com>
2019-03-06 23:59:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 /* Enter the code to remove the main node element under this comment */
-document.
+document.main.remove()
+
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
+document
