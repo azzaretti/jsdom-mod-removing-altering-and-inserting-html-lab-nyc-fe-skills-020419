241800e108bd3e2dfe50fd52cf72eda14d1b0130
Greg Azzaretti <azzaretti.greg@gmail.com>
2019-03-07 00:18:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let myMain = document.querySelector('#main')
 myMain.remove()
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = document.createElement(h1);
+let newHeader = document.createElement(h1);
 
 newHeader.id = "victory"
 newHeader.innerHTML = "Greg is the Champion!"
