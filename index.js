753fd0b61d11e0bf94969399b8cd20323f75ecb3
Greg Azzaretti <azzaretti.greg@gmail.com>
2019-03-07 00:14:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ myMain.remove()
 /* Create your new element here and assign it to newHeader */
 const newHeader = document.createElement(h1);
 
-
+newHeader.id = "victory"
+newHeader.innerHTML = "Greg is the Champion!"
 
 
